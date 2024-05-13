ba576c10d30e9e999119a1e8731544a664ac5d96
Eduardo <eportugalpor@unsa.edu.pe>
2024-05-13 12:51:00 -0500

    Ejemplo 7 de w3schools

---
+++ Ejemplos w3schools/Ejemplo07/gethint.php
@@ -0,0 +1,56 @@
+<?php
+// Array de nombres para sugerencias
+$a[] = "Anna";
+$a[] = "Brittany";
+$a[] = "Cinderella";
+$a[] = "Diana";
+$a[] = "Eva";
+$a[] = "Fiona";
+$a[] = "Gunda";
+$a[] = "Hege";
+$a[] = "Inga";
+$a[] = "Johanna";
+$a[] = "Kitty";
+$a[] = "Linda";
+$a[] = "Nina";
+$a[] = "Ophelia";
+$a[] = "Petunia";
+$a[] = "Amanda";
+$a[] = "Raquel";
+$a[] = "Cindy";
+$a[] = "Doris";
+$a[] = "Eve";
+$a[] = "Evita";
+$a[] = "Sunniva";
+$a[] = "Tove";
+$a[] = "Unni";
+$a[] = "Violet";
+$a[] = "Liza";
+$a[] = "Elizabeth";
+$a[] = "Ellen";
+$a[] = "Wenche";
+$a[] = "Vicky";
+
+// Obtener el parámetro q enviado por la solicitud
+$q = $_REQUEST["q"];
+
+$hint = "";
+
+// Buscar coincidencias desde el principio de los nombres
+if ($q !== "") {
+    $q = strtolower($q);
+    $len = strlen($q);
+    foreach($a as $name) {
+        if (stristr($q, substr($name, 0, $len))) {
+            if ($hint === "") {
+                $hint = $name;
+            } else {
+                $hint .= ", $name";
+            }
+        }
+    }
+}
+
+// Imprimir respuesta o "no suggestion" si no hay sugerencias
+echo $hint === "" ? "no suggestion" : $hint;
+?>
