5bba08a394b1d0e7691cd50f52730a3b825ea526
Eduardo <eportugalpor@unsa.edu.pe>
2024-05-16 20:36:00 -0500

    script del problema 7 - funcion drawGoogleChart() para crear, dibujar y mostrar los graficos comparativos entre regiones

--- Tarea Ajax y Google Chart/7.js
+++ Tarea Ajax y Google Chart/7.js
@@ -63,3 +63,40 @@ function drawChart() {
     xmlhttp.open("GET", "data.json", true);
     xmlhttp.send();
 }
+
+function drawGoogleChart(datasets, dates) {
+    google.charts.load('current', {'packages':['corechart']});
+    google.charts.setOnLoadCallback(function() {
+        var chartsContainer = document.getElementById("graphics");
+        chartsContainer.innerHTML = "";
+
+        Object.keys(datasets).forEach(section => {
+            var data = new google.visualization.DataTable();
+            data.addColumn('string', 'Date');
+            datasets[section].forEach(dataset => {
+                data.addColumn('number', dataset.label);
+            });
+
+            var rows = [];
+            dates.forEach(date => {
+                var row = [date];
+                datasets[section].forEach(dataset => {
+                    var value = dataset.data[dates.indexOf(date)] || 0;
+                    row.push(value);
+                });
+                rows.push(row);
+            });
+            data.addRows(rows);
+
+            var options = {
+                title: section.toUpperCase(),
+                curveType: 'function',
+                legend: { position: 'bottom' }
+            };
+
+            var chart = new google.visualization.LineChart(document.createElement('div'));
+            chart.draw(data, options);
+            chartsContainer.appendChild(chart.getContainer());
+        });
+    });
+}
