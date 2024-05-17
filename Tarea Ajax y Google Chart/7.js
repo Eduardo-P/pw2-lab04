39b511a7714c214c81df9b0b79bcebaa11e4a292
Eduardo <eportugalpor@unsa.edu.pe>
2024-05-16 19:55:00 -0500

    script del problema 7 - funcion drawChart() que obtiene todo los datos

--- Tarea Ajax y Google Chart/7.js
+++ Tarea Ajax y Google Chart/7.js
@@ -21,3 +21,45 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+function drawChart() {
+    var selectedRegions = Array.from(document.getElementById("select_region").selectedOptions).map(option => option.value);
+
+    var datasets = {};
+    var dates = [];
+
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+            var data = JSON.parse(xmlhttp.responseText);
+
+            selectedRegions.forEach(regionName => {
+                var regionData = data.find(region => region.region === regionName);
+                var color = '#' + (Math.random().toString(16) + '000000').substring(2,8);
+
+                Object.keys(regionData).forEach(section => {
+                    if (Array.isArray(regionData[section])) {
+                        if (!datasets[section]) {
+                            datasets[section] = [];
+                        }
+
+                        var values = regionData[section].map(entry => parseInt(entry.value));
+                        var sectionDates = regionData[section].map(entry => entry.date);
+                        dates = dates.concat(sectionDates);
+
+                        datasets[section].push({
+                            label: regionName,
+                            data: values,
+                            color: color
+                        });
+                    }
+                });
+            });
+
+            dates = [...new Set(dates)];
+
+            drawGoogleChart(datasets, dates);
+        }
+    };
+    xmlhttp.open("GET", "data.json", true);
+    xmlhttp.send();
+}
